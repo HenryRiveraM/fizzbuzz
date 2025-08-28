e95213c00bd46d89723b5c47edd241b55cbd46fc
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-27 22:51:24 -0400

    feat: agregando lo avanzado en clase

---
+++ src/fizzbuzz.js
@@ -0,0 +1,9 @@
+function convertirFizzBuzz(n){
+    if (n % 3 === 0){
+        return "Fizz";
+    }
+
+    return n + "";
+}
+
+export default convertirFizzBuzz;
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,18 @@
+import { covertirFizzBuzz } from "./fizzbuzz";
+
+describe ("Fizzbuzz", () => {
+    it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
+        expect(covertirFizzBuzz(1)).toEqual("1");
+    });
+
+    it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
+        expect(covertirFizzBuzz(4)).toEqual("4");
+    });
+
+    // numero multiplo de 3 ejem 3 -> 'Fizz'
+    it("deberia convertir un numero que es multiplo de 3 ", () => {
+        expect(covertirFizzBuzz(3)).toEqual("Fizz");
+    });
+
+});
+
