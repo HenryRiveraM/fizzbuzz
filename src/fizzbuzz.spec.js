864b23838c9ecb1e91ca083dd7b3b3ed8b7eeedf
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-27 23:10:08 -0400

    feat: agregando la ultima prueba de los multiplos 3 y 5 fizzbuzz

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,24 +1,28 @@
 import convertirFizzBuzz from "./fizzbuzz";
 
-describe ("Fizzbuzz", () => {
+describe("Fizzbuzz", () => {
+    // numero que no es multiplo de 3 ni 5 ejem 1 -> '1'
     it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
-        expect(covertirFizzBuzz(1)).toEqual("1");
+        expect(convertirFizzBuzz(1)).toEqual("1");
     });
 
+    // numero que no es multiplo de 3 ni 5 ejem 4 -> '4'
     it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
-        expect(covertirFizzBuzz(4)).toEqual("4");
+        expect(convertirFizzBuzz(4)).toEqual("4");
     });
 
     // numero multiplo de 3 ejem 3 -> 'Fizz'
     it("deberia convertir un numero que es multiplo de 3 ", () => {
-        expect(covertirFizzBuzz(3)).toEqual("Fizz");
+        expect(convertirFizzBuzz(3)).toEqual("Fizz");
     });
-
 
     // numero multiplo de 5 ejem 5 -> 'Buzz'
     it("deberia convertir un numero que es multiplo de 5 ", () => {
         expect(convertirFizzBuzz(5)).toEqual("Buzz");
     });
 
+    // numero multiplo de 3 y 5 ejem 15 -> 'FizzBuzz'
+    it("deberia convertir un numero que es multiplo de 3 y 5 ", () => {
+        expect(convertirFizzBuzz(15)).toEqual("FizzBuzz");
+    });
 });
-
