93de8925e4b8dba629d7bb26fb1caa90fe867f9d
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-27 23:06:12 -0400

    feat: agregando la prueba de multiplo 5 buzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,21 @@
-function convertirFizzBuzz(n){
-    if (n % 3 === 0){
+
+function convertirFizzBuzz(n) {
+    // multiplo de 3 y 5
+    if (n % 3 === 0 && n % 5 === 0) {
+        return "FizzBuzz";
+    }
+
+    // multiplo de 3
+    if (n % 3 === 0) {
         return "Fizz";
     }
 
+    // multiplo de 5
+    if (n % 5 === 0) {
+        return "Buzz";
+    }
+
+    // no es multiplo de 3 ni 5
     return n + "";
 }
 
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,4 +1,4 @@
-import { covertirFizzBuzz } from "./fizzbuzz";
+import convertirFizzBuzz from "./fizzbuzz";
 
 describe ("Fizzbuzz", () => {
     it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
@@ -12,6 +12,12 @@ describe ("Fizzbuzz", () => {
     // numero multiplo de 3 ejem 3 -> 'Fizz'
     it("deberia convertir un numero que es multiplo de 3 ", () => {
         expect(covertirFizzBuzz(3)).toEqual("Fizz");
+    });
+
+
+    // numero multiplo de 5 ejem 5 -> 'Buzz'
+    it("deberia convertir un numero que es multiplo de 5 ", () => {
+        expect(convertirFizzBuzz(5)).toEqual("Buzz");
     });
 
 });
